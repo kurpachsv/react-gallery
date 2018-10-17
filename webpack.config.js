fc6ee89f98d4bb2602a4a109018dcad3b9bebc88
kurpachsv <kurpachsv@gmail.com>
2018-10-17 11:47:11 +0300

    Prepare to publish version 0.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'style.css',
-        }),
-    ],
-    entry: {
-        app: './src/index.js',
-    },
+    mode: 'production',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'index.js',
+        libraryTarget: 'commonjs',
     },
     module: {
         rules: [
@@ -25,9 +19,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract(
-                    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]'
-                ),
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]',
             },
         ],
     },
