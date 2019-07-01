fbf87cb12b736efd03613f4922a796eb0f925acc
kurpachsv <kurpachsv@gmail.com>
2019-07-01 16:10:25 +0300

    migrate to styled-components

--- src/Image.js
+++ src/Image.js
@@ -1,72 +1,61 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styles from './image.css';
+
+import {
+    ImageWithoutSizes,
+    ImageWithSizes,
+} from './nodes';
 
 const Image = ({
-    style,
     className,
     src,
     alt,
     visible,
     height,
     width,
-    newWidth,
-    newHeight,
-    newWidthInPercent,
-    placeholderHeight,
-    enableMasonry,
-    maxHeight,
-    maxWidth,
     fixedBottom,
     specifyImageSizes,
-    ...rest
+    ...props,
 }) => {
     if (specifyImageSizes) {
         const ratio = width / height;
         return (
-            <img
-                {...rest}
-                className={`${styles.image} ${className}`}
+            <ImageWithSizes
+                className={className}
                 src={visible ? src : null}
                 alt={alt}
-                style={{
-                    display: visible ? null : 'none',
-                    position: 'absolute',
-                    width: ratio > 1 ? '100%' : 'auto',
-                    height: ratio > 1 ? 'auto' : `calc(100% - ${fixedBottom}px)`,
-                    ...style,
-                }}
+                ratio={ratio}
+                visible={visible}
+                height={height}
+                width={width}
+                fixedBottom={fixedBottom}
+                onClick={props.onClick}
             />
         );
     } else {
         return (
-            <img
-                {...rest}
-                className={`${styles.image} ${className}`}
+            <ImageWithoutSizes
+                className={className}
                 src={visible ? src : null}
                 alt={alt}
-                style={{
-                    display: visible ? null : 'none',
-                    width: '100%',
-                    position: 'absolute',
-                    ...style,
-                }}
+                visible={visible}
+                onClick={props.onClick}
             />
         );
     }
 };
 
 Image.propTypes = {
-    className: PropTypes.string,
     src: PropTypes.string.isRequired,
     alt: PropTypes.string,
     visible: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 
 Image.defaultProps = {
-    className: '',
     alt: '',
     visible: true,
+    onClick: () => {},
 };
 
 export default Image;
---
+++ src/nodes.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Image = ({className, src, alt, onClick}) => (
+    <img className={className} src={src} alt={alt} onClick={onClick} />
+);
+
+export const Div = ({className, children}) => (
+    <div className={className}>
+        {children}
+    </div>
+);
+
+export const ImageWithSizes = styled(Image)`
+    position: absolute;
+    display: ${props => props.visible ? null : 'none'};
+    width: ${props => props.ratio > 1 ? '100%' : 'auto'};
+    height: ${props => props.radio > 1 ? 'auto' : `calc(100% - ${props.fixedBottom || 0}px)`};
+    cursor: pointer;
+`;
+
+export const ImageWithoutSizes = styled(Image)`
+    position: absolute;
+    display: ${props => props.visible ? null : 'none'};
+    width: 100%;
+    cursor: pointer;
+`;
+
+export const Container = styled(Div)`
+    display: block;
+    font-size: 0;
+`;
+
+export const Item = styled(Div)`
+    vertical-align: top;
+    position: relative;
+    display: inline-block;
+`;
+
+export const ItemMasonry = styled(Item)`
+    width: ${props => `${100 / props.columnsMaxCount - props.gutterInPercent}%`};
+    margin: ${props => `0 ${props.gutterInPercent}% 0 0`};
+`;
+
+export const ItemDefault = styled(Item)`
+    width: ${props => props.isIncomplete && !props.disableLastRowDetecting ? `${props.newWidth}px` : `${props.newWidthInPercent}%`};
+    max-width: ${props => props.isIncomplete && !props.disableLastRowDetecting ? `${props.newWidthInPercent}%` : 'auto'};
+    margin: ${props => props.rowLength === props.columnIndex + 1
+        ? `0 0 ${props.gutterInPercent}% 0`
+        : `0 ${props.gutterInPercent}% ${props.gutterInPercent}% 0`};
+`;
+
+
+export const ItemFixed = styled(Item)`
+    background-color: rgb(187, 189, 191);
+    width: ${props => props.isIncomplete ? `${props.newWidth}px` : `${props.newWidthInPercent}%`};
+    margin: ${props => props.rowLength === props.columnIndex + 1
+        ? `0 0 ${props.gutterInPercent}% 0`
+        : `0 ${props.gutterInPercent}% ${props.gutterInPercent}% 0`};
+`;
+
+export const DetailsContainer = styled.div`
+    height: ${props => props.visible ? `${props.height}px` : '0'};
+    font-size: 14px;
+    visibility: ${props => props.visible ? 'visible' : 'hidden'};
+    margin-bottom: ${props => props.visible ? `${props.gutterInPercent}%` : 0};
+`;
+
+export const DetailsImageWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const DetailsImage = styled(Image)`
+    height: ${props => `${props.height}px`};
+    width: ${props => `${props.width}px`};
+`;
+
